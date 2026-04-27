ddb658a911151cc4e66f9ca7d2d787bba929b385
Eklavya Sharma <ekurgn@gmail.com>
2026-04-27 12:30:54 -0500

    add width and height to images

--- render.py
+++ render.py
@@ -1,11 +1,27 @@
 #!/usr/bin/env python3
+
 import os
+import sys
 import json
 from os.path import join as pjoin
 import argparse
 import jinja2
+from typing import Any
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+def addImgDims(entries: list[dict[str, Any]], imgDir: str) -> None:
+    try:
+        from PIL import Image
+    except ImportError:
+        print('Package Pillow is not installed. Not computing width and height of images.', file=sys.stderr)
+        return
+
+    for entry in entries:
+        fpath = pjoin(imgDir, entry['fname'])
+        with Image.open(fpath) as img:
+            entry['width'], entry['height'] = img.size
 
 
 def main() -> None:
@@ -14,8 +30,8 @@ def main() -> None:
         help='Path to Jinja2 template (default: <repo>/index.html.jinja2)')
     parser.add_argument('--entries', default=pjoin(ROOT_DIR, 'entries.json'),
         help='Path to entries in JSON (default: <repo>/entries.json)')
-    # parser.add_argument('--img-dir', default=pjoin(ROOT_DIR, 'img'),
-        # help='Path to directory containing images (default: <repo>/img)')
+    parser.add_argument('--img-dir', default=pjoin(ROOT_DIR, 'img'),
+        help='Path to directory containing images (default: <repo>/img)')
     parser.add_argument('-o', '--output', default=pjoin(ROOT_DIR, 'index.html'),
         help='Path to output html file (default: <repo>/index.html)')
     args = parser.parse_args()
@@ -24,6 +40,7 @@ def main() -> None:
         entries = json.load(fp)
     with open(args.template) as fp:
         template = jinja2.Template(fp.read())
+    addImgDims(entries, args.img_dir)
     output = template.render({'entries': entries})
     with open(args.output, 'w') as fp:
         fp.write(output)
